ea7e5eeddbe3f2270130a38993db5a176d7e22a1
viktoriiakoniukhova <viktoriia.koniukhova@gmail.com>
2023-04-11 07:47:15 +0300

    Modal window - now is viewport height

--- client/src/components/ui/Modal/Modal.jsx
+++ client/src/components/ui/Modal/Modal.jsx
@@ -79,94 +79,99 @@ export default function Modal({ isShowing, hide, productData }) {
                   className={styles.modal}
                   onClick={(e) => e.stopPropagation()}
                 >
-                  <div className={styles.modalHeader}>
-                    <Button
-                      type="darkBlue"
-                      hasArrow={false}
-                      text="×"
-                      onClick={hide}
-                    />
-                  </div>
-                  <div className={styles.productCardDetails}>
-                    <div className={styles.productCardDetailsContent}>
-                      <div
-                        className={styles.imgContainer}
-                        style={{ backgroundImage: `url(${imgUrl})` }}
-                      >
-                        <div className={styles.categoryInfo}>
-                          <p>{category.name}</p>
-                        </div>
-                      </div>
-                      <div className={styles.content}>
-                        <h3>{name}</h3>
-                        <div className={styles.imgContainer}>
-                          <img src={rating} alt="rate" />
-                        </div>
-                        <div className={styles.contentInfo}>
-                          <div className={styles.price}>
-                            {hasDiscount && (
-                              <p className={styles.discountPrice}>
-                                <s>{currencyFilter(price + discount)}</s>
-                              </p>
-                            )}
-                            <h3>{currencyFilter(price)}</h3>
-                          </div>
-                          <p>{desc}</p>
-                          <div className={styles.quantity}>
-                            <h3>Quantity:</h3>
-                            <input
-                              type="text"
-                              placeholder={orderQuantity}
-                              value={orderQuantity}
-                              onChange={(e) => updateQuantity(+e.target.value)}
-                            />
-                            {!isEnough && <p>Sorry, not enough product.</p>}
-                            <Button
-                              hasArrow={true}
-                              type="darkBlue"
-                              text="Add To Cart"
-                              onClick={addToCart}
-                            />
-                          </div>
-                        </div>
-                      </div>
+                  <div className={styles.modalContent}>
+                    {" "}
+                    <div className={styles.modalHeader}>
+                      <Button
+                        type="darkBlue"
+                        hasArrow={false}
+                        text="×"
+                        onClick={hide}
+                      />
                     </div>
-                    <div className={styles.productCardDetailsInfo}>
-                      <div className={styles.productCardInfoRadio}>
-                        <input
-                          type="radio"
-                          name="info"
-                          value="desc"
-                          id="desc"
-                          checked={info === "desc"}
-                          onChange={(e) => setInfo(e.target.value)}
-                        />
-                        <label
-                          className={
-                            info === "desc" ? `${styles.active}` : undefined
-                          }
-                          htmlFor="desc"
+                    <div className={styles.productCardDetails}>
+                      <div className={styles.productCardDetailsContent}>
+                        <div
+                          className={styles.imgContainer}
+                          style={{ backgroundImage: `url(${imgUrl})` }}
                         >
-                          <h3>Product Description</h3>
-                        </label>
-                        <input
-                          type="radio"
-                          name="info"
-                          value="add"
-                          id="add"
-                          checked={info === "add"}
-                          onChange={(e) => setInfo(e.target.value)}
-                        />
-                        <label
-                          className={
-                            info === "add" ? `${styles.active}` : undefined
-                          }
-                          htmlFor="add"
-                        >
-                          <h3>Additional Info</h3>
-                        </label>
+                          <div className={styles.categoryInfo}>
+                            <p>{category.name}</p>
+                          </div>
+                        </div>
+                        <div className={styles.content}>
+                          <h3>{name}</h3>
+                          <div className={styles.imgContainer}>
+                            <img src={rating} alt="rate" />
+                          </div>
+                          <div className={styles.contentInfo}>
+                            <div className={styles.price}>
+                              {hasDiscount && (
+                                <p className={styles.discountPrice}>
+                                  <s>{currencyFilter(price + discount)}</s>
+                                </p>
+                              )}
+                              <h3>{currencyFilter(price)}</h3>
+                            </div>
+                            <p>{desc}</p>
+                            <div className={styles.quantity}>
+                              <h3>Quantity:</h3>
+                              <input
+                                type="text"
+                                placeholder={orderQuantity}
+                                value={orderQuantity}
+                                onChange={(e) =>
+                                  updateQuantity(+e.target.value)
+                                }
+                              />
+                              {!isEnough && <p>Sorry, not enough product.</p>}
+                              <Button
+                                hasArrow={true}
+                                type="darkBlue"
+                                text="Add To Cart"
+                                onClick={addToCart}
+                              />
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <p>{info === "desc" ? desc : category.desc}</p>
+                      <div className={styles.productCardDetailsInfo}>
+                        <div className={styles.productCardInfoRadio}>
+                          <input
+                            type="radio"
+                            name="info"
+                            value="desc"
+                            id="desc"
+                            checked={info === "desc"}
+                            onChange={(e) => setInfo(e.target.value)}
+                          />
+                          <label
+                            className={
+                              info === "desc" ? `${styles.active}` : undefined
+                            }
+                            htmlFor="desc"
+                          >
+                            <h3>Product Description</h3>
+                          </label>
+                          <input
+                            type="radio"
+                            name="info"
+                            value="add"
+                            id="add"
+                            checked={info === "add"}
+                            onChange={(e) => setInfo(e.target.value)}
+                          />
+                          <label
+                            className={
+                              info === "add" ? `${styles.active}` : undefined
+                            }
+                            htmlFor="add"
+                          >
+                            <h3>Additional Info</h3>
+                          </label>
+                        </div>
+                        <p>{info === "desc" ? desc : category.desc}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
