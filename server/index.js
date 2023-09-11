65b9571350ed6ff700a98cad2ba62491d538b557
viktoriiakoniukhova <viktoriia.koniukhova@gmail.com>
2023-09-11 16:33:44 +0300

    update server settings for deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,4 @@
-const ATLAS_URI =
-  "mongodb+srv://tory:DHKLa3Mx4m-e.Qj@cluster0.3b27q1x.mongodb.net/groceryShop?retryWrites=true&w=majority";
-const PORT = 3001;
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -9,7 +6,30 @@ const app = express();
 const ProductModel = require("./models/Product");
 const OrderModel = require("./models/Order");
 
-app.use(cors());
+const ATLAS_URI = process.env.MONGO_DB;
+const PORT = 3001;
+
+// app.use(cors());
+
+// Set middleware of CORS
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gentle-creponne-7351f1.netlify.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 app.use(express.json());
 
 mongoose.connect(ATLAS_URI, {
